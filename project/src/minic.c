ca29bf6e3674d566662f0e9dbc97cca28f0fb896
Afnan Enayet <afnan@afnan.io>
2018-10-07 19:46:47 -0400

    Create function stub
    Create function stub for semantic analysis

--- project/src/minic.c
+++ project/src/minic.c
@@ -9,6 +9,19 @@
 #include "ast.h"
 #include "parser.h"
 
+/**
+ * \brief Return whether the AST is semantically valid.
+ *
+ * Walk through the tree and make sure that variables are declared before they
+ * are used, and that variables aren't declared multiple times.
+ *
+ * \param[in] tree the root node of the AST
+ * \return whether the AST is semantically valid
+ */
+bool semantic_check(ast_node_t *tree) {
+    return false;
+}
+
 int main(void) {
     // create ast root
     ast_root = create_node_type(T_MAIN);

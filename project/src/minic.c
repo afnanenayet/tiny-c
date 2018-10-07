03b628357c0cc1f8fa4034c54b1d60d1afff74b9
Afnan Enayet <afnan@afnan.io>
2018-10-07 19:39:10 -0400

    Print diagnostic info, update lexer to omit warns

--- project/src/minic.c
+++ project/src/minic.c
@@ -4,14 +4,15 @@
  * Main entry point for executable
  */
 
+#include<stdio.h>
+
 #include "ast.h"
+#include "parser.h"
 
 int main(void) {
-    // create extern root
-    // yyparse() takes input
-    // arbitrary number of children (NOT a btree)
-    // bison has the actions that make the tree
-    // never have to explicitly call yylex
+    // create ast root
+    ast_root = create_node_type(T_MAIN);
     yyparse();
+    printf("%d children\n", ast_root->num_children);
     return 0;
 }

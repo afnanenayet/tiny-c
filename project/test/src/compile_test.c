38ca3f8a152c9b528ed6cdee52ca8031cd39aea8
Afnan Enayet <afnan@afnan.io>
2018-10-08 15:05:40 -0400

    Update build files for testing
    * generate library from minic
    * update testing build file

--- project/test/src/compile_test.c
+++ project/test/src/compile_test.c
@@ -1,9 +1,10 @@
 /* test_compile.c
  * Afnan Enayet 2018
  *
- * This file tests whether ast.h compiles correctly.
+ * This file tests whether the C APIs compile correctly
  */
 
 #include "ast.h"
+#include "vector.h"
 
 int main(void) { return 0; }

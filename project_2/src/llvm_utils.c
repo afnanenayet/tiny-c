4b8bcc4f170a524c02b1527162d72fe81d6355b6
Afnan Enayet <afnan@afnan.io>
2018-10-30 14:54:45 -0400

    Update build files, add genKill method
    * update cmake files to make doxygen dependency optional/opt-in
    * initial work on genKill method

--- project_2/src/llvm_utils.c
+++ project_2/src/llvm_utils.c
@@ -11,6 +11,7 @@
 #include <llvm-c/IRReader.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "llvm_utils.h"
 #include "print_utils.h"
@@ -66,8 +67,8 @@ val_vec_t *computeGenSet(LLVMBasicBlockRef bb) {
     // initialize the empty set
     // need to malloc because the library assumes the vector is already
     // allocated and doesn't initialize the struct for you
-    val_vec_t *bbSet = malloc(sizeof(val_vec_t));
-    vec_init(bbSet);
+    val_vec_t *genSet = malloc(sizeof(val_vec_t));
+    vec_init(genSet);
 
     // iterate over the instructions in the basic block
     for (LLVMValueRef inst = LLVMGetFirstInstruction(bb); inst;
@@ -75,9 +76,9 @@ val_vec_t *computeGenSet(LLVMBasicBlockRef bb) {
         // if the instruction is a store instruction, store it in the set
         if (LLVMIsAStoreInst(inst)) {
 #ifdef DEBUG
-            println("found store instruction");
+            println("(gen set) found store instruction");
 #endif
-            vec_push(bbSet, inst);
+            vec_push(genSet, inst);
         }
         // get memory location of the store instruction
         LLVMValueRef newStoreLoc = LLVMGetOperand(inst, 1);
@@ -86,16 +87,38 @@ val_vec_t *computeGenSet(LLVMBasicBlockRef bb) {
         // vector reference the same location, if so, remove them
         int i;
         LLVMValueRef val;
-        vec_foreach(bbSet, val, i) {
+        vec_foreach(genSet, val, i) {
             LLVMValueRef oldStoreLoc = LLVMGetOperand(val, 1);
 
             if (oldStoreLoc == newStoreLoc) {
 #ifdef DEBUG
-                println("redundant location");
+                println("(gen set) redundant location");
 #endif
-                vec_remove(bbSet, val);
+                vec_remove(genSet, val);
             }
         }
     }
-    return bbSet;
+    return genSet;
+}
+
+val_vec_t *computeKillSet(LLVMBasicBlockRef bb) {
+    val_vec_t *killSet = malloc(sizeof(val_vec_t));
+    vec_init(killSet);
+    val_vec_t *storeSet = malloc(sizeof(val_vec_t));
+    vec_init(storeSet);
+
+    // compute some set S of all the constant store instructions in some
+    // given function
+    for (LLVMValueRef inst = LLVMGetFirstInstruction(bb); inst;
+         inst = LLVMGetNextInstruction(inst)) {
+        if (LLVMIsAStoreInst(inst)) {
+#ifdef DEBUG
+            println("(kill set) Found a store instruction");
+#endif
+            vec_push(storeSet, inst);
+        }
+    }
+    vec_deinit(storeSet);
+    free(storeSet);
+    return killSet;
 }

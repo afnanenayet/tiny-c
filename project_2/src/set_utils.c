6db4e0ef294fa145f27014f99f11fda6c2424840
Afnan Enayet <afnan@afnan.io>
2018-10-31 12:50:43 -0400

    Implement set union, dedup methods

--- project_2/src/set_utils.c
+++ project_2/src/set_utils.c
@@ -1,1 +1,36 @@
 #include "set_utils.h"
+#include "vec.h"
+
+vec_void_t *setUnion(vec_void_t *a, vec_void_t *b) {
+    vec_void_t tempSet;
+    vec_init(&tempSet);
+    int i;
+    void *it;
+
+    // add all elements from set a
+    vec_foreach(a, it, i) { vec_push(&tempSet, it); }
+
+    // add all elements from set b
+    vec_foreach(b, it, i) { vec_push(&tempSet, it); }
+    vec_void_t *dedupedSet = setDedup(&tempSet);
+    vec_deinit(&tempSet);
+    return dedupedSet;
+}
+
+vec_void_t *setDedup(vec_void_t *a) {
+    vec_void_t *newSet = malloc(sizeof(vec_void_t));
+    vec_init(newSet);
+    void *it;
+    int i = 0;
+    int found = -1;
+
+    // Create a new vector, checking to see if each element can be found to
+    // avoid adding duplicates
+    vec_foreach(a, it, i) {
+        vec_find(newSet, it, found);
+
+        if (found == -1)
+            vec_push(newSet, it);
+    }
+    return newSet;
+}

88dc00669de9f45632082587d1d4bd8ae8c7cbe5
Afnan Enayet <afnan@afnan.io>
2018-11-11 23:45:00 -0500

    Update LLVM utils, update main module
    * Update documentation for LLVM util functions
    * Update main module to parse the given file

--- project_3/include/LLVMUtils.h
+++ project_3/include/LLVMUtils.h
@@ -19,7 +19,10 @@
  * parse the file using LLVM's utilities.
  *
  * It will throw an exception if there are any errors or if the filepath
- * is invalid.
+ * is invalid. This will not verify the file, however. If the file is
+ * malformed but LLVM can still parse it (perhaps the file is not well
+ * formed), it is the responsibility of the user to verify that everything
+ * is working.
  *
  * \param filename The relative path to the file
  * \param context The MT-safe LLVM context. There should only be one per
--- project_3/src/main.cpp
+++ project_3/src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
 #include <llvm/IRReader/IRReader.h>
+
+#include "LLVMUtils.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         std::cerr << "Need to supply LLVM IR file!\n";
         return 1;
     }
+
+    // parse module file
+    auto context = llvm::LLVMContext();
+    auto module = loadModule(argv[1], context);
     return 0;
 }

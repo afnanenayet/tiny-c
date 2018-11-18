0ac71ef0991ffc63816830267585f24bb1fd8a59
Afnan Enayet <afnan@afnan.io>
2018-11-17 22:04:57 -0500

    Update header, define functions
    * update header file with proper function prototypes
    * define functions to create index, offset tables

--- project_3/include/LLVMUtils.h
+++ project_3/include/LLVMUtils.h
@@ -7,10 +7,52 @@
  * Convenience/helper functions that help parse and interact with LLVM IR in
  * the context of global register allocation
  */
-#include <llvm/IRReader/IRReader.h>
+#include <set>
 #include <string>
+#include <tuple>
+#include <unordered_map>
+
+#include <llvm/IR/Instruction.h>
+#include <llvm/IRReader/IRReader.h>
 
 #pragma once
+
+/*** typedefs ***/
+
+//! The types of physical registers available
+typedef enum {
+    eax,
+    ebx,
+    ecx,
+    edx,
+} PhysicalRegister;
+
+//! A mapping from an instruction to its byte offset
+typedef std::unordered_map<const llvm::Instruction *, int> OffsetTable;
+
+//! A mapping from an instruction to its index
+typedef std::unordered_map<const llvm::Instruction *, unsigned int> IndexTable;
+
+/*!
+ * \brief A table from instructions to their liveliness intervals
+ *
+ * A mapping from an LLVM instruction to its liveness interval (a tuple of two
+ * integers that represent a variable from when it's live to when it dies)
+ */
+typedef std::unordered_map<const llvm::Instruction *, std::tuple<int, int>>
+    IntervalTable;
+
+/*!
+ * \brief Temporary register to physical register table.
+ *
+ * A mapping from an temporary instruction/register to the corresponding
+ * available physical registers.
+ */
+typedef std::unordered_map<std::shared_ptr<llvm::Instruction>,
+                           std::set<PhysicalRegister>>
+    RegisterTable;
+
+/*** function prototypes ***/
 
 /*!
  * \brief Create an LLVM IR module given the filename of some LLVM IR file.
@@ -31,3 +73,43 @@
  */
 std::unique_ptr<llvm::Module> loadModule(const std::string &filename,
                                          llvm::LLVMContext &context);
+
+/*!
+ * \brief Create the instruction offset table for an LLVM function
+ *
+ * Given some LLVM function, this method will construct the offset table for
+ * each instruction in the function. This table will store the offsets for
+ * each variable and the return value.
+ *
+ * These offsets represent the offset that needs to be added to `%ebp` when
+ * generating the code.
+ *
+ * \param func[in] The LLVM function to inspect
+ * \returns An offset table
+ */
+std::shared_ptr<OffsetTable> genOffsetTable(const llvm::Function &func);
+
+/*!
+ * \brief Create an index table with indices for each basic block
+ *
+ * Given some basic block, generate a mapping of instructions to
+ * their corresponding index offset
+ *
+ * \param[in] bb The basic block to inspect
+ * \returns An IndexTable with the offsets for each instruction
+ */
+std::shared_ptr<IndexTable> genIndexTable(const llvm::BasicBlock &bb);
+
+/*!
+ * \brief Create the liveliness table for each instruction in a basic block
+ *
+ * Given some basic block, this method will generate the table to determine
+ * how long each instruction is "alive" for, and will store that as a tuple
+ *
+ * \param[in] bb The basic block to inspect
+ * \param[in] The populated index table
+ * \returns A table that contains the live interval for each instruction
+ */
+std::shared_ptr<IntervalTable>
+genIntervalTable(const llvm::BasicBlock &bb,
+                 const std::shared_ptr<IndexTable> indexTable);
--- project_3/src/LLVMUtils.cpp
+++ project_3/src/LLVMUtils.cpp
@@ -1,11 +1,17 @@
 #include <filesystem>
+#include <memory>
+#include <stdexcept>
+#include <string.h>
+#include <unordered_map>
+
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IRReader/IRReader.h>
 #include <llvm/Support/ErrorOr.h>
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Support/SourceMgr.h>
-#include <stdexcept>
 
 #include "LLVMUtils.h"
 
@@ -17,4 +23,45 @@ std::unique_ptr<llvm::Module> loadModule(const std::string &filename,
     if (module == nullptr)
         throw std::runtime_error("Could not parse IR file");
     return module;
+}
+
+std::shared_ptr<OffsetTable> genOffsetTable(const llvm::Function &func) {
+    auto table = std::make_shared<OffsetTable>();
+    int offset = -4;
+
+    // get the entry basic block
+    const auto &entryBB = func.getEntryBlock();
+
+    for (const auto &inst : entryBB) {
+        if (strcmp(inst.getOpcodeName(), "alloca") == 0) {
+            table->insert(std::make_pair(&inst, offset));
+            offset -= 4;
+        }
+    }
+    return table;
+}
+
+std::shared_ptr<IndexTable> genIndexTable(const llvm::BasicBlock &bb) {
+    auto table = std::make_shared<IndexTable>();
+
+    // the counter for the instructions
+    unsigned i = 0;
+
+    // iterate through each instruction, only incrementing if the instruction
+    // is not an `alloc` instruction
+    // Note: the example code does not check to see if an instruction is an
+    // `alloca` instruction, so I did not do so here
+    for (const auto &inst : bb) {
+        table->insert(std::make_pair(&inst, i++));
+    }
+    return table;
+}
+
+std::shared_ptr<IntervalTable>
+genIntervalTable(const llvm::BasicBlock &bb,
+                 const std::shared_ptr<IndexTable> indexTable) {
+    auto table = std::make_shared<IntervalTable>();
+    // iterate backwards through the instructions
+    // TODO
+    return table;
 }
